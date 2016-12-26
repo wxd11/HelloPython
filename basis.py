5949298a132dc887656073aacea102d5f03d3dd4
wxd <wxd11@gmail.com>
2016-12-26 22:25:00 +0800

    rename hello->input_print add basis

---
+++ basis.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#python2
+flag=True;
+if 3<2:
+     print flag
+else:
+     print not flag
+
+
+ord('c');
+chr(100);
+u'bc'.encode('utf-8');
+len('wwef');
+'abc'.decode('utf-8');
+
+#python3
---
+++ input_print.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print 'Hello Python';
+
+print 'test', 'space', 'space';
+
+print 300;
+
+print 100+200*3/2;
+
+print ('hello word');
+
+name=raw_input('please enter your name:');
+
+print name;
+
+age=input('年龄: ')
+print (age)
